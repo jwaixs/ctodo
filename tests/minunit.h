742c675711da6381f5bd75678128fff6ddb91f49
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2013-01-21 08:44:06 +0100

    Better code design trick copied from git/alloc.c.

--- tests/minunit.h
+++ tests/minunit.h
@@ -8,20 +8,34 @@
 
 #define mu_suite_start() char *message = NULL;
 
-#define mu_assert(test, message) { if (!(test)) return message; }
-#define mu_run_test(test) printf("TESTING %s\n", #test); \
-	message = test(); tests_run++; if (message) return message;
-#define RUN_TESTS(name) int main(int argc, char *argv[]) { \
-	argc = 1; \
-	printf("===== RUNNING: %s =====\n", argv[0]); \
-	char *result = name(); \
-	if (result != 0) { \
-		printf("FAILED: %s\n", result); \
-	} else { \
-		printf("All tests passed!\n"); \
-	}\
-	printf("Tests run: %d\n", tests_run); \
-	return result != 0; \
+#define mu_assert(test, message) 				\
+{ 								\
+	if (!(test)) 						\
+		return message; 				\
+}
+
+#define mu_run_test(test)					\
+{								\
+	printf("TESTING %s\n", #test); 				\
+	message = test(); 					\
+	tests_run++; 						\
+	if (message) 						\
+		return message;					\
+}
+
+#define RUN_TESTS(name) 					\
+int main(int argc, char *argv[]) 				\
+{ 								\
+	argc = 1; 						\
+	printf("===== RUNNING: %s =====\n", argv[0]); 		\
+	char *result = name(); 					\
+	if (result != 0) { 					\
+		printf("FAILED: %s\n", result); 		\
+	} else { 						\
+		printf("All tests passed!\n"); 			\
+	}							\
+	printf("Tests run: %d\n", tests_run); 			\
+	return result != 0; 					\
 }
 
 int tests_run;
